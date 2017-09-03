15eab64d891a56fafa951830d00f0363b3ba58b9
Erika Smith <erikaannesmith@gmail.com>
2017-09-02 18:29:15 -0600

    Add logic for ship
    Now the ship knows which space it occupies! Ahoyyyyyy matey.

--- lib/ship.rb
+++ lib/ship.rb
@@ -4,4 +4,25 @@ class Ship
   def initialize(coordinate_range)
     @coordinate_range = coordinate_range
   end
+
+  def occupies?(coordinate)
+    coordinates.include?(coordinate)
+  end
+
+  private
+
+  def coordinates
+    split_coordinates = coordinate_range.split(" ")
+    bow = split_coordinates[0]
+    stern = split_coordinates[-1]
+    x_line = (bow[0]..stern[0])
+    y_line = (bow[-1]..stern[-1])
+
+    GameBoard::COORDINATES.select do |coordinate|
+      x = coordinate[0]
+      y = coordinate[-1]
+
+      x_line.include?(x) && y_line.include?(y)
+    end
+  end
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,4 +9,28 @@ class ShipTest < MiniTest::Test
 
     assert_instance_of Ship, ship
   end
+
+  def test_occupies_coordinate_when_coordinate_occupied_horizontally
+    ship = Ship.new("A1 A3")
+
+    assert ship.occupies?("A1")
+    assert ship.occupies?("A2")
+    assert ship.occupies?("A3")
+  end
+
+  def test_occupies_coordinate_when_coordinate_not_occupied_horizontally
+    ship = Ship.new("A1 A3")
+
+    refute ship.occupies?("A4")
+    refute ship.occupies?("B2")
+  end
+
+  def test_occupies_coordinate_when_coordinate_occupied_vertically
+    ship = Ship.new("A1 C1")
+
+    assert ship.occupies?("A1")
+    assert ship.occupies?("B1")
+    assert ship.occupies?("C1")
+  end
+
 end
